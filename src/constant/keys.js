4ff67e1f305a57089d1f8ed39b12fd05aec0c3ff
M Shafique Qadri <mshafiqueqadri@iShafique.local>
2018-05-31 13:49:15 +0500

    Structure

---
+++ src/constant/keys.js
@@ -0,0 +1,3 @@
+export const BTC = 'BackToSchool';
+export const PROFILE = `${BTC}:user:profile`;
+export const USER_ID = `${BTC}:user:id`;
---
+++ src/environment/index.js
@@ -0,0 +1,2 @@
+export const MAIN_DOMAIN = ``;
+export const API = ``;
