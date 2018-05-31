b42aeb1104aa5001a78a7e993d7317eda783581b
uliaquat <usman.liaquat@barefoot-studio.be>
2018-05-31 16:52:18 +0500

    initail dev dependencies req

--- src/pages/Auth/Signup.vue
+++ src/pages/Auth/Signup.vue
@@ -1,10 +1,16 @@
 <template>
     <div id="singup">
         <h1>singup</h1>
+        <form>
+            <input id="" v-model="user.email" class=""/>
+            <input v-model="user.password" class=""/>
+            <input v-model="user.confirm_password" class=""/>
+        </form>
     </div>
 </template>
 
 <script>
+import '../../assets/scss/style.scss';
     export default {
         name: 'singup'
     }
